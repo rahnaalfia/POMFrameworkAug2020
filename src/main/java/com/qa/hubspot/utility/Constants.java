8c435806f23be81a90be9f1ee0a09b5b8ee58c1e
Rahna Alfia <rahnaalfia@Rahnas-MacBook-Pro.local>
2020-08-20 07:34:58 +0530

    New URL

--- src/main/java/com/qa/hubspot/utility/Constants.java
+++ src/main/java/com/qa/hubspot/utility/Constants.java
@@ -3,9 +3,9 @@ package com.qa.hubspot.utility;
 public class Constants {
 
 	public static final String LOGIN_PAGE_TITLE = "HubSpot Login";
-	public static final String HOME_PAGE_TITLE = "Accounts Dashboard Beta | HubSpot";
+	public static final String HOME_PAGE_TITLE = "Account Setup | HubSpot";
 	public static final String INVALID_HOME_PAGE_HEADER = "Invalid HubSpot Accounts";
-	public static final String HOME_PAGE_HEADER = "HubSpot Accounts";
+	public static final String HOME_PAGE_HEADER = "Setup Guide";
 	public static final String CONTACTS_SHEET_NAME = "contacts";
 
 }
